2a42bc179816f2c8b7d226e1393e6869ac5d2925
Jonathan Battisti <jonathan.battisti@inspq.qc.ca>
2019-11-01 16:25:38 -0400

    Ajoute tests data-list component

--- src/app/lieu-vaccination-avance/data-list/data-list.component.spec.ts
+++ src/app/lieu-vaccination-avance/data-list/data-list.component.spec.ts
@@ -2,13 +2,18 @@ import { Shallow } from 'shallow-render';
 import { LieuVaccinationAvanceModule } from '../lieu-vaccination-avance.module';
 import { DataListComponent } from './data-list.component';
 
-describe('<app-champ-texte />', () => {
+describe('<app-data-list />', () => {
   let shallow: Shallow<DataListComponent>;
 
   beforeEach(() => {
     shallow = new Shallow(DataListComponent, LieuVaccinationAvanceModule);
   });
 
+  it('doit afficher données injectées', async () => {
+    const noms = ['Québec', 'Trois-Rivières', 'Laval'];
+    const { instance } = await shallow.render({ bind: { noms } });
+    expect(instance.noms).toEqual(noms);
+  });
 
 });
 
--- src/app/lieu-vaccination-avance/data-list/data-list.component.ts
+++ src/app/lieu-vaccination-avance/data-list/data-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-data-list',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./data-list.component.css']
 })
 export class DataListComponent implements OnInit {
+
+  @Input() noms: string[] = [];
 
   constructor() { }
 
