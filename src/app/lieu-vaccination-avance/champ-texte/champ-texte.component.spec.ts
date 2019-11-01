efb9e9893997f162c10d7e294455ecc3191a0104
Jonathan Battisti <jonathan.battisti@inspq.qc.ca>
2019-11-01 15:34:13 -0400

    Ajoute component champ-texte

--- src/app/lieu-vaccination-avance/champ-texte/champ-texte.component.spec.ts
+++ src/app/lieu-vaccination-avance/champ-texte/champ-texte.component.spec.ts
@@ -2,15 +2,27 @@ import { Shallow } from 'shallow-render';
 import { ChampTexteComponent } from './champ-texte.component';
 import { LieuVaccinationAvanceModule } from '../lieu-vaccination-avance.module';
 
-
-
-describe('ChampTexteComponent', () => {
+describe('<app-champ-texte />', () => {
   let shallow: Shallow<ChampTexteComponent>;
 
   beforeEach(() => {
     shallow = new Shallow(ChampTexteComponent, LieuVaccinationAvanceModule);
   });
 
+  it('doit afficher donnée injecté', async () => {
+    const { find } = await shallow.render('<app-champ-texte texte="Québec"></app-champ-texte>');
+    expect(find('[data-testid="champ-texte"]').nativeElement.value).toBe('Québec');
+  });
 
+  describe('sur l\'événement input', () => {
+
+    it('doit retourner la valeur du champs', async () => {
+      const { find, instance } = await shallow.render();
+      find('[data-testid="champ-texte"]').nativeElement.value = 'Laval';
+      find('[data-testid="champ-texte"]').triggerEventHandler('input', { target: find('[data-testid="champ-texte"]').nativeElement });
+      expect(instance.texteInput.emit).toHaveBeenCalledWith('Laval');
+    });
+
+  });
 
 });
--- src/app/lieu-vaccination-avance/champ-texte/champ-texte.component.ts
+++ src/app/lieu-vaccination-avance/champ-texte/champ-texte.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-champ-texte',
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChampTexteComponent implements OnInit {
 
+  @Input() texte = '';
+  @Output() texteInput: EventEmitter<string> = new EventEmitter();
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  emitTexteInput(event: any) {
+    this.texteInput.emit(event.target.value);
+  }
 }
